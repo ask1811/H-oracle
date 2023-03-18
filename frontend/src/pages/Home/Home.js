2100ae298d1958f83860491d52f527979fed775c
joshuajoyreji <joshuajoyr@gmail.com>
2023-03-19 03:20:49 +0530

    Home

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -6,14 +6,19 @@ const Home = () => {
         <>
             <div className='content'>
                 <div className='container'>
-                    <p className='details'>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-                    mollit anim id est laborum.
-                    </p>
+                    <div className='row details'>
+                        <p className=' col-6'>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+                        mollit anim id est laborum.
+                        </p>
+                        <p className='col-3 offset-1'> To check health, enter details here<button type="button" class="btn btn-danger "><a href='/Details' className='dtl'>Details</a></button></p>
+                        
+                    </div>
+                    
                 </div>
                 
             </div>
