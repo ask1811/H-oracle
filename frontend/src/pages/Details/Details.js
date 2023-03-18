32d8b81e7bb51fbf1fc91d202cb2c7753cd94db0
joshuajoyreji <joshuajoyr@gmail.com>
2023-03-18 23:20:19 +0530

    Details

--- frontend/src/pages/Details/Details.js
+++ frontend/src/pages/Details/Details.js
@@ -1,46 +1,112 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
-// import './details.css';
+import './details.css';
 function Details(){
     return(
         <div className='container'>
             <div className='row'>
-                <h1>Enter the following details</h1>
+                <h1 className='heading'>Enter the following details</h1>
             </div>
             <br/>
-            <div className='row'>
-            <form class="form-horizontal">
-                <div class="form-group">
-                    <label class="control-label col-sm-2" for="pwd">Age:</label>
-                    <div class="col-sm-4">
-                        <input type="number" class="form-control" id="pwd" placeholder="In Years" min='1'/>
+            <form>
+                <div class="form-group row">
+                    <label class="control-label col-sm-2" for="age">Age:</label>
+                    <div class="col-sm-3">
+                        <input type="number" class="form-control" id="age" placeholder="In Years" min='1'/>
                     </div>
-                </div>
-                <br/>
-                <div class="form-group">
-                    <label class="control-label col-sm-2" for="pwd">Gender:</label>
-                    <div class="col-sm-4">
-                        <select class="form-control">
+                    <label class="control-label col-sm-2 offset-1" for="gender">Gender:</label>
+                    <div class="col-sm-3">
+                        <select class="form-control" id='gender'>
                             <option value="male">Male</option>
                             <option value="female">Female</option>
                         </select>
                     </div>
                 </div>
                 <br/>
+                <div class="form-group row">
+                    <label className='control-label col-sm-2'>Blood pressure:</label>
+                    <div class="col-sm-3">
+                        <input type="text" class="form-control"/>
+                    </div>
+                    <label className='control-label col-sm-2 offset-1'>Serum cholestoral in mg/dl:</label>
+                    <div class="col-sm-3">
+                        <input type="text" class="form-control"/>
+                    </div>
+                </div>
+                <br/>
+                <div class="form-group row">
+                    <label className='control-label col-sm-2'>Max heart rate:</label>
+                    <div class="col-sm-3">
+                        <input type="text" class="form-control"/>
+                    </div>
+                    <label className='control-label col-sm-2 offset-1'>Exercise induced angina:</label>
+                    <div class="col-sm-3">
+                        <input type="text" class="form-control"/>
+                    </div>
+                </div>
+                <br/>
+                <div class="form-group row">
+                    <label className='control-label col-sm-2'>Oldpeak:</label>
+                    <div class="col-sm-3">
+                        <input type="text" class="form-control"/>
+                    </div>
+                    <label className='control-label col-sm-2 offset-1'>Slope ST:</label>
+                    <div class="col-sm-3">
+                        <input type="text" class="form-control"/>
+                    </div>
+                </div>
+                <br/>
+                <div class="form-group row">
+                    <label className='control-label col-sm-2'>Thall:</label>
+                    <div class="col-sm-3">
+                        <input type="text" class="form-control"/>
+                    </div>
+                </div>
+                <br/>
                 <div class="form-group">
-                    <label class="control-label col-1" for="pwd">Chest pain:</label>
-                    <br/>
-                    <input type="radio" id="html" name="cp" value="0"/>
-                    <label class="col-1" for="html">0</label>
-                    <input type="radio" id="css" name="cp" value="1"/>
-                    <label class="col-1" for="css">1</label>
-                    <input type="radio" id="javascript" name="cp" value="2"/>
-                    <label class="col-1" for="javascript">2</label>
-                    <input type="radio" id="javascript" name="cp" value="3"/>
-                    <label class="col-1" for="javascript">3</label>
+                    <label class="control-label col-2" for="pwd">No of major vessels:</label>
+                    <input type="radio" id="0" name="cp" value="0"/>
+                    <label class="col-1" for="0">0</label>
+                    <input type="radio" id="1" name="cp" value="1"/>
+                    <label class="col-1" for="1">1</label>
+                    <input type="radio" id="2" name="cp" value="2"/>
+                    <label class="col-1" for="2">2</label>
+                    <input type="radio" id="3" name="cp" value="3"/>
+                    <label class="col-1" for="3">3</label>
+                </div>
+                <br/>
+                <div class="form-group">
+                    <label class="control-label col-2" for="pwd">Fasting blood sugar more than 120 mg/dl?:</label>
+                    <input type="radio" id="true" name="cp" value="true"/>
+                    <label class="col-1" for="true">true</label>
+                    <input type="radio" id="false" name="cp" value="false"/>
+                    <label class="col-1" for="false">false</label>
+                </div>
+                <br/>
+                <div class="form-group">
+                    <label class="control-label col-2" for="pwd">Chest pain:</label>
+                    <input type="radio" id="0" name="cp" value="0"/>
+                    <label class="col-1" for="0">0</label>
+                    <input type="radio" id="1" name="cp" value="1"/>
+                    <label class="col-1" for="1">1</label>
+                    <input type="radio" id="2" name="cp" value="2"/>
+                    <label class="col-1" for="2">2</label>
+                    <input type="radio" id="3" name="cp" value="3"/>
+                    <label class="col-1" for="3">3</label>
+                </div>
+                <br/>
+                <div class="form-group">
+                    <label class="control-label col-2" for="pwd">Resting electrocardiographic result:</label>
+                    <input type="radio" id="0" name="cp" value="0"/>
+                    <label class="col-1" for="0">0</label>
+                    <input type="radio" id="1" name="cp" value="1"/>
+                    <label class="col-1" for="1">1</label>
+                    <input type="radio" id="2" name="cp" value="2"/>
+                    <label class="col-1" for="2">2</label>
+                    <input type="radio" id="3" name="cp" value="3"/>
+                    <label class="col-1" for="3">3</label>
                 </div>
             </form>
-            </div>
         </div>
     )
 }
