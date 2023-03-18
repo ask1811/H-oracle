97888b997cd50fcfd0a14031e7d3daa0281d5fcd
Ashwin S Kuamar <ashwnskmr@gmail.com>
2023-03-18 19:05:06 +0530

    commit

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter,Route,Routes }  from 'react-router-dom';
+import Home from './pages/Home/home'; 
+import Signup from './pages/Signup/signup'; 
+import Navbar from './components/Navbar/navbar';
+import Login from './pages/Login/login'; 
+function App() {
+return (
+  <div className="App">
+    <BrowserRouter>
+  	<Navbar/>
+  	<Routes>
+  		<Route path='/' element={<Home/>} />
+  		 <Route path='/login' element={<Login/>} />
+  		 <Route path='/signup' element={<Signup/>} />
+	
+
+  	</Routes>
+    </BrowserRouter>
+  </div>
+);
+}
+
+export default App;
