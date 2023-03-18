9465fd97b8fabac6a1c9212d4374749c7c7ff8f4
Ashwin S Kuamar <ashwnskmr@gmail.com>
2023-03-18 21:49:58 +0530

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter,Route,Routes }  from 'react-router-dom';
-//import Home from './pages/Home/home'; 
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
 //import Signup from './pages/Signup/signup'; 
 import Navbar from './Components/Navbar/Navbar';
-//import Login from './pages/Login/login'; 
-import Details from './pages/Details/Details'; 
+import Login from './pages/Login/Login';
+import Details from './pages/Details/Details';
 
 function App() {
-return (
-  <div className="App">
-    <BrowserRouter>
-  	<Navbar/>
-  	<Routes>
-  		
-      <Route path='/Details' element={<Details/>} />
-	  </Routes>
-    </BrowserRouter>
-  </div>
-);
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/Details' element={<Details />} />
+          <Route path='/Login' element={<Login />} />
+
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -1,11 +1,52 @@
 import React from 'react'
-
+import './Login.css'
 const Login = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div>
+
+            <div className="content">
+                <form name="my" onSubmit="usePage(this,event)" action="" class="form">
+                    <h2>Log In</h2>
+                    <div className="input-group">
+                        <input type="text" name="email" id="loginUser" />
+                        <label for="loginUser">E-mail</label>
+                    </div>
+                    <div className="input-group">
+                        <input
+                            type="password"
+                            name="password"
+                            id="loginPassword"
+
+                            required
+                        />
+                        <label for="loginPassword">Password</label>
+                    </div>
+                    <p>Please select user</p>
+                    <input type="radio" id="owner" name="user" value="Owner" onChange={() => {
+                    }} />
+                    <label for="owner">Owner</label>
+                    <input type="radio" id="tenant" name="user" value="Tenant" onChange={() => {
+                    }} />
+                    <label for="tenant">Tenant</label>
+                    {
+                        <button type="submit" className="submit-btn" onClick={(e) => {
+                            e.preventDefault();
+                        }}>Login</button>
+                    }
+                    {
+                        <button type="submit" className="submit-btn" onClick={(e) => {
+                            e.preventDefault();
+
+                        }}>Login</button>
+                    }
+
+                </form>
+                <a className='sig' href="/signup" target="_self">Click here to signup</a>
+
+
+            </div>
+        </div>
+    )
 }
 
 export default Login
