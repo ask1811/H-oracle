c6ed2a64772c26ab3cf0ca760fd0a8c152e4dc70
Ashwin S Kuamar <ashwnskmr@gmail.com>
2023-03-19 03:58:44 +0530

    Commit

--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -5,33 +5,34 @@ import { Link } from 'react-router-dom'
 
 const Login = () => {
     return (
+        <div className='motham'>
+            <div className='login-wrapper container'>
+                <div >
+                    <form name="my" class="form">
+                        <h2>Log In</h2>
+                        <div className="input-group">
+                            <input type="text" name="email" id="loginUser" required />
+                            <label for="loginUser">E-mail</label>
+                        </div>
+                        <div className="input-group">
+                            <input
+                                type="password"
+                                name="password"
+                                id="loginPassword"
+                                required
+                            />
+                            <label for="loginPassword">Password</label>
+                        </div>
+                        <button type="submit" className="submit-btn" >Login</button>
 
-        <div className='login-wrapper container'>
-            <div >
-                <form name="my" class="form">
-                    <h2>Log In</h2>
-                    <div className="input-group">
-                        <input type="text" name="email" id="loginUser" required />
-                        <label for="loginUser">E-mail</label>
-                    </div>
-                    <div className="input-group">
-                        <input
-                            type="password"
-                            name="password"
-                            id="loginPassword"
-                            required
-                        />
-                        <label for="loginPassword">Password</label>
-                    </div>
-                    <button type="submit" className="submit-btn" >Login</button>
+                    </form>
+                    <br />
+                    <p className='sig'>Do not have an account Click Here to Signup</p>
+                    <Link to="/Signup"><button classname='btn ' variant=" contained" >Click Here</button></Link>
 
-                </form>
-                <br />
-                <p className='sig'>Do not have an account</p>
-                <Link to="/Signup"><button classname='btn ' variant=" contained" >Click Here</button></Link>
-
+                </div >
             </div >
-        </div >
+        </div>
     )
 }
 
