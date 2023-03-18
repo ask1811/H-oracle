ab40ed3b434cedf4eb90a85378d5755da8b4165f
Ashwin S Kuamar <ashwnskmr@gmail.com>
2023-03-19 02:13:12 +0530

    Commit

--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -3,33 +3,33 @@ import './Navbar.css';
 import translogo from '../../Images/translogo.png'
 
 const Navbar = () => {
-  return (
-    <div class="nav">
-      <input type="checkbox" id="nav-check" />
-      <div class="nav-header">
-        <img class='logo' src={translogo} />
-        <div class="nav-title">
-          <a href="/" target="_self" className='btn0'>
-            <span class="replies">H-oracle</span>
-          </a>
-        </div>
-      </div>
-      <div class="nav-btn">
-        <label for="nav-check">
-          <span></span>
-          <span></span>
-          <span></span>
-        </label>
-      </div>
+    return (
+        <div class="nav">
+            <input type="checkbox" id="nav-check" />
+            <div class="nav-header">
+                <img class='logo' src={translogo} />
+                <div class="nav-title">
+                    <a href="/" target="_self" className='btn0'>
+                        <span class="replies">H-oracle</span>
+                    </a>
+                </div>
+            </div>
+            <div class="nav-btn">
+                <label for="nav-check">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </label>
+            </div>
 
-      <div class="nav-links">
-        <a href="/login" target="_self" className='btn1'>
-          <span class="replies">Login</span>
-          <span class="comments"></span>
-        </a>
-      </div>
-    </div >
-  )
+            <div class="nav-links">
+                <a href="/login" target="_self" className='btn1'>
+                    <span class="replies">Login</span>
+                    <span class="comments"></span>
+                </a>
+            </div>
+        </div >
+    )
 }
 
 export default Navbar
