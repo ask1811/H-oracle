8b5dd6781bc80719e06725f460f3b3c23f465f46
joshuajoyreji <joshuajoyr@gmail.com>
2023-03-18 19:05:41 +0530

    page

---
+++ frontend/src/pages/details/detail.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import './details.css';
+function details(){
+    return(
+        <div>
+            <div>
+                <h1>Enter the following details</h1>
+            </div>
+            <div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default details;
