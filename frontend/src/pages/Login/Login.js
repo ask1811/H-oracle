32db7218ed55772cf8af5ec9b1f23a6e21f430ab
joshuajoyreji <joshuajoyr@gmail.com>
2023-03-19 00:44:40 +0530

    Login

--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import './Login.css'
+import 'bootstrap/dist/css/bootstrap.css';
+
 const Login = () => {
     return (
-        <div>
-
-            <div className="content">
-                <form name="my" onSubmit="usePage(this,event)" action="" class="form">
+        
+        <div className='login-wrapper container'>
+            <div className="content ">
+                <form name="my" class="form">
                     <h2>Log In</h2>
                     <div className="input-group">
-                        <input type="text" name="email" id="loginUser" />
+                        <input type="text" name="email" id="loginUser" required/>
                         <label for="loginUser">E-mail</label>
                     </div>
                     <div className="input-group">
@@ -16,25 +18,18 @@ const Login = () => {
                             type="password"
                             name="password"
                             id="loginPassword"
-
                             required
                         />
                         <label for="loginPassword">Password</label>
                     </div>
-
-                    {
-                        <button type="submit" className="submit-btn" onClick={(e) => {
-                            e.preventDefault();
-                        }}>Login</button>
-                    }
+                    <button type="submit" className="submit-btn" >Login</button>                 
 
                 </form>
+                <br/>
                 <a className='sig' href="/signup" target="_self">Click here to signup</a>
-
-
             </div>
         </div>
     )
 }
 
-export default Login
+export default Login;
