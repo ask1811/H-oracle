510ca3a8fa97f0bcd1d1f1fe507fc494c48b991e
Ashwin S Kuamar <ashwnskmr@gmail.com>
2023-03-19 09:17:17 +0530

    Commit

--- backend/index.js
+++ backend/index.js
@@ -3,30 +3,30 @@ const app = express();
 const mongoose = require("mongoose");
 const { MONGO_DB_CONFIG } = require("./config/app.config");
 require("dotenv").config();
-const cors=require('cors');
+const cors = require('cors');
 const port = process.env.PORT;
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(cors());
 //const swaggerUi = require("swagger-ui-express"), swaggerDocument = require("./swagger.json");
 
-mongoose.connect(MONGO_DB_CONFIG.DB,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
+mongoose.connect("mongodb+srv://arg007:Freelancer%402022@cluster0.gp52g.mongodb.net/technohack?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 })
-.then(
-    ()=>{
-        console.log("Database connected");
-    },
-    (error)=>{
-        console.log("Database not connected"+error);
-    }
-);
+    .then(
+        () => {
+            console.log("Database connected");
+        },
+        (error) => {
+            console.log("Database not connected" + error);
+        }
+    );
 
 app.use(express.json());
-app.use("/api/auth",require("./routes/auth.routes"));
-app.use("/api/heart",require("./routes/heart.routes"));
+app.use("/api/auth", require("./routes/auth.routes"));
+app.use("/api/heart", require("./routes/heart.routes"));
 
-app.listen(process.env.port || 4000, ()=>{
+app.listen(process.env.port || 4000, () => {
     console.log("Server running")
 })
--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -2,30 +2,30 @@ const express = require("express");
 const router = express.Router();
 const Users = require("../model/user.model");
 
-router.get('',(req,res)=>{
+router.get('', (req, res) => {
     res.json("Hello")
 })
 
-router.post('/signup',async(req,res)=>{
-        const name = req.body.name;
-        const email = req.body.email;
-        const password = req.body.password;
-        const user = new Users({name,email,password});
-        const storeData = await user.save();
-        res.status(201).json(storeData);
-    })
+router.post('/signup', async (req, res) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
+    const user = new Users({ name, email, password });
+    const storeData = await user.save();
+    res.status(201).json(storeData);
+})
 
-router.post("/signin",async(req,res)=>{
+router.post("/signin", async (req, res) => {
     var email = req.body.email;
     var password = req.body.password;
-    var user = await Users.find({"email":email})
+    var user = await Users.find({ "email": email })
     console.log(user);
-    if (user[0].password==password){
-        res.status(201).json({"message":"authorised","token":"xxx","data":user});
+    if (user[0].password == password) {
+        res.status(201).json({ "message": "authorised", "token": "xxx", "data": user });
     }
-    else{
-        res.status(401).json({"messge":"unauthorised"})
-    }  
+    else {
+        res.status(401).json({ "messge": "unauthorised" })
+    }
 })
 
 module.exports = router;
