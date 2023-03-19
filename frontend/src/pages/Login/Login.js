91cffaccaf0cd5e8d443374af449faf4993b5331
Ashwin S Kuamar <ashwnskmr@gmail.com>
2023-03-19 09:14:55 +0530

    Commit

--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState, Link } from 'react'
+import axios from "axios";
+import { Input } from 'reactstrap'
 import './Login.css'
-import 'bootstrap/dist/css/bootstrap.css';
-import { Link } from 'react-router-dom'
+
+const baseURL = "https://localhost:4000/api"
 
 const Login = () => {
+    const [logdata, setData] = useState({
+        email: "",
+        password: ""
+    })
+    const addData = (e) => {
+        // console.log(e.target);
+        const { name, value } = e.target;
+        setData(() => {
+            return {
+                ...logdata,
+                [name]: value
+            }
+
+        })
+    }
+    async function login() {
+        await axios
+            .post(`${baseURL}/signin`, {
+                username: logdata.username,
+                password: logdata.password
+            })
+            .then((response) => {
+                console.log(response.data);
+                localStorage.setItem("token", response.data.token);
+                localStorage.setItem("userId", response.data.data[0]._id)
+                window.location.href = "/";
+            });
+    }
+
     return (
         <div className='motham'>
             <div className='login-wrapper container'>
@@ -11,7 +42,7 @@ const Login = () => {
                     <form name="my" class="form">
                         <h2>Log In</h2>
                         <div className="input-group">
-                            <input type="text" name="email" id="loginUser" required />
+                            <input type="text" name="email" id="loginUser" onChange={addData} value={logdata.username} required />
                             <label for="loginUser">E-mail</label>
                         </div>
                         <div className="input-group">
@@ -19,11 +50,16 @@ const Login = () => {
                                 type="password"
                                 name="password"
                                 id="loginPassword"
+                                onChange={addData} value={logdata.password}
                                 required
                             />
                             <label for="loginPassword">Password</label>
                         </div>
-                        <button type="submit" className="submit-btn" >Login</button>
+                        <Input type="submit" value="Sign Up" class="submit-btn" onClick={(e) => {
+                            e.preventDefault();
+                            console.log(logdata);
+                            signup()
+                        }} />
 
                     </form>
                     <br />
--- frontend/src/pages/Signup/Signup.js
+++ frontend/src/pages/Signup/Signup.js
@@ -1,6 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from "axios";
+import { Input } from 'reactstrap'
+
 import './Signup.css'
+const baseURL = "https://localhost:4000/api"
+
 const Signup = () => {
+    const [logdata, setData] = useState({
+        name: "",
+        email: "",
+        password: ""
+    })
+
+    const addData = (e) => {
+        const { name, value } = e.target;
+        setData(() => {
+            return {
+                ...logdata,
+                [name]: value
+            }
+
+        })
+    }
+
+    async function signup() {
+        await axios
+            .post(`${baseURL}/signup`, {
+                name: logdata.name,
+                email: logdata.email,
+                password: logdata.password
+            })
+            .then((response) => {
+                console.log(response.data);
+                localStorage.setItem("token", response.data.token);
+                window.location.href = "/";
+            });
+    }
     return (
         <div className='motham'>
             <div className='signup-wrapper container'>
@@ -8,11 +43,11 @@ const Signup = () => {
                     <form name="my" class="form">
                         <h2>Sign Up</h2>
                         <div className="input-group">
-                            <input type="text" name="name" id="loginUser" />
+                            <input type="text" name="name" id="loginUser" onChange={addData} value={logdata.name} />
                             <label for="loginUser">Name</label>
                         </div>
                         <div className="input-group">
-                            <input type="text" name="email" id="signupUser" />
+                            <input type="text" name="email" id="signupUser" onChange={addData} value={logdata.username} />
                             <label for="signupUser">E-mail</label>
                         </div>
                         <div className="input-group">
@@ -20,14 +55,18 @@ const Signup = () => {
                                 type="password"
                                 name="password"
                                 id="loginPassword"
-
+                                onChange={addData} value={logdata.password}
                                 required
                             />
                             <label for="loginPassword">Password</label>
                         </div>
 
                         {
-                            <button type="submit" className="submit-btn" >Signup</button>
+                            <Input type="submit" value="Sign Up" class="submit-btn" onClick={(e) => {
+                                e.preventDefault();
+                                console.log(logdata);
+                                signup()
+                            }} />
                         }
 
                     </form>
