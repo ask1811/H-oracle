8163e071d1d337333c6d54aa1b5cf3d1afa3153a
Ashwin S Kuamar <ashwnskmr@gmail.com>
2023-03-19 06:48:53 +0530

    Commit

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -2,28 +2,55 @@ import React from 'react'
 import './Home.css'
 import 'bootstrap/dist/css/bootstrap.css'
 const Home = () => {
-    return (
-        <>
-            <div className='content'>
-                <div className='container'>
-                    <div className='row details'>
-                        <p className=' col-6'>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-                        mollit anim id est laborum.
-                        </p>
-                        <p className='col-3 offset-1'> To check health, enter details here<button type="button" class="btn btn-danger "><a href='/Details' className='dtl'>Details</a></button></p>
-                        
+    if (localStorage.getItem("token") == "xxx") {
+        return (
+            <>
+                <div className='content'>
+                    <div className='container'>
+                        <div className='row details'>
+                            <p className=' col-6'>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+                                mollit anim id est laborum.
+                            </p>
+                            <p className='col-3 offset-1'> To check health, enter details here<button type="button" class="btn btn-danger "><a href='/Details' className='dtl'>Details</a></button></p>
+
+                        </div>
+
                     </div>
-                    
+
                 </div>
-                
-            </div>
-        </>
-    )
+            </>
+        )
+    }
+
+
+
+    else {
+        return (
+            <>
+                <div className='content'>
+                    <div className='container'>
+                        <div className='row details'>
+                            <p className=' col-6'>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+                                mollit anim id est laborum.
+                            </p>
+                        </div>
+
+                    </div>
+
+                </div>
+            </>
+        )
+    }
 }
 
 export default Home
